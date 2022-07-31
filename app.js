0d61d96a24df3b49ef752dc598aec002752849fa
Piyush Agrawal <piyush200205@gmail.com>
2022-07-31 15:50:33 +0530

    added routes

--- app.js
+++ app.js
@@ -17,9 +17,25 @@ const handlebars = exphbs.create({ extname: '.hbs',});
 app.engine('.hbs', handlebars.engine);
 app.set('view engine', '.hbs');
 
-//Router
-app.get('', (req, res) => {
-    res.render('home');
+
+const pool = mysql.createPool({
+    connectionLimit : 100,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
+
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('Connected as id ' + connection.threadId);
+    connection.release();
+});
+
+const routes = require('./server/routes/user.js');
+app.use('/', routes);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
