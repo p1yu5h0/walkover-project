cb86c545026663956c304e3e790aff827702ff81
Piyush Agrawal <piyush200205@gmail.com>
2022-08-02 13:04:51 +0530

    added till submit functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,48 +1,88 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const pool = mysql.createPool({
-    connectionLimit : 100,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+  connectionLimit: 100,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-
 exports.view = (req, res) => {
-    
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("Connected as id " + connection.threadId);
 
-    pool.getConnection((err, connection) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log('Connected as id ' + connection.threadId);
-
-        //user connection
-        connection.query('SELECT * FROM user', (err, rows)=>{
-
-            //when done with the connection, release it
-            connection.release();
-            if(!err){
-                res.render('home', { rows });
-            } else {
-                console.log(err);
-            }
-            console.log('The data from user table: \n', rows)
-        });
-    });
-}
-
-exports.find = (req, res) => {
-    let searchTerm = req.body.search;
-    // User the connection
-    connection.query('SELECT * FROM user WHERE first_name LIKE ? OR last_name LIKE ?', ['%' + searchTerm + '%', '%' + searchTerm + '%'], (err, rows) => {
+    //user connection
+    connection.query("SELECT * FROM user", (err, rows) => {
+      //when done with the connection, release it
+      connection.release();
       if (!err) {
-        res.render('home', { rows });
+        res.render("home", { rows });
       } else {
         console.log(err);
       }
-      console.log('The data from user table: \n', rows);
+      console.log("The data from user table: \n", rows);
     });
-  }
+  });
+};
+
+exports.find = (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("Connected as id " + connection.threadId);
+
+    let searchTerm = req.body.search;
+    // User the connection
+    connection.query(
+      "SELECT * FROM user WHERE first_name LIKE ? OR last_name LIKE ?",
+      ["%" + searchTerm + "%", "%" + searchTerm + "%"],
+      (err, rows) => {
+        if (!err) {
+          res.render("home", { rows });
+        } else {
+          console.log(err);
+        }
+        console.log("The data from user table: \n", rows);
+      }
+    );
+  });
+};
+
+exports.form = (req, res) => {
+  res.render("add-user");
+};
+
+exports.create = (req, res) => {
+  const { first_name, last_name, email, phone, comments } = req.body;
+  // res.render('add-user');
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("Connected as id " + connection.threadId);
+
+    let searchTerm = req.body.search;
+    // User the connection
+    connection.query(
+      "INSERT INTO user SET first_name = ?, last_name = ?, email = ?, phone = ?, comments = ?",
+      [first_name, last_name, email, phone, comments],
+      (err, rows) => {
+        connection.release();
+        if (!err) {
+          res.render("add-user");
+        } else {
+          console.log(err);
+        }
+        console.log("The data from user table: \n", rows);
+      }
+    );
+  });
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -5,8 +5,8 @@ const userController = require('../controllers/userController');
 // Routes
 router.get('/', userController.view);
 router.post('/', userController.find);
-// router.get('/adduser', userController.form);
-// router.post('/adduser', userController.create);
+router.get('/adduser', userController.form);
+router.post('/adduser', userController.create);
 // router.get('/edituser/:id', userController.edit);
 // router.post('/edituser/:id', userController.update);
 // router.get('/viewuser/:id', userController.viewall);
